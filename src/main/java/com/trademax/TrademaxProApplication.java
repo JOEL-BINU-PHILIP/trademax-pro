517c1223f7e140d3e50ba10fe505f41c3bc16ee6
JOEL-BINU-PHILIP <joelbinuphilip@gmail.com>
2025-11-27 17:35:24 +0530

    First Commit

---
+++ src/main/java/com/trademax/TrademaxProApplication.java
@@ -0,0 +1,13 @@
+package com.trademax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrademaxProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrademaxProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trademax/TrademaxProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trademax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrademaxProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
